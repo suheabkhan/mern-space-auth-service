a787539e63ebc71f0d7728537746d23dc39d376c
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2023-09-18 16:03:06 +0530

    add eslint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        //This property is to avoid clash between eslint and prettier, after installing the npm dependency
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    root: true,
+    rules: {
+        'no-console': 'error',
+        'dot-notation': 'error',
+    },
+};
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+function login(username: string): string {
+    return username;
+}
+
+login('codersgyan');
