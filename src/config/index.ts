383c8d9b1668a319463b57572d1e1fa07cde10dd
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-01-02 11:10:48 +0530

    setup typeorm

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,19 @@
 import { config } from 'dotenv';
 
-config();
+import path from 'path';
 
-const { PORT, NODE_ENV } = process.env;
+// This is to ensure , the .env files are read different for each environment
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_USERNAME,
+    DB_PASSWORD,
 };
