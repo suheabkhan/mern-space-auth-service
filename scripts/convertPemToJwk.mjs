a9148e586195e0189275d3879ca98bd76904b485
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-01-06 21:03:43 +0530

    removed console

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -6,5 +6,3 @@ const privateKey = fs.readFileSync('./certs/private.pem');
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const jwk = rsaPemToJwk(privateKey, { use: 'sig' }, 'public');
-
-console.log(JSON.stringify(jwk));
