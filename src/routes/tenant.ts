3a3d57cbaf735c609f5a2f14ed956a0f493414bc
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-01-07 12:53:01 +0530

    authorize test for tenant creation

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -5,14 +5,18 @@ import { AppDataSource } from '../config/data-source';
 import { Tenant } from '../entity/Tenant';
 import { createTenantRequest } from '../types/index';
 import logger from '../config/logger';
+import authenticationMiddleware from '../middleware/authenticationMiddleware';
 
 const router = express.Router();
 const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
 const tenantController = new TenantController(tenantService, logger);
 
-router.post('/', async (req: Request, res: Response, next: NextFunction) =>
-    tenantController.create(req as createTenantRequest, res, next),
+router.post(
+    '/',
+    authenticationMiddleware,
+    async (req: Request, res: Response, next: NextFunction) =>
+        tenantController.create(req as createTenantRequest, res, next),
 );
 
 export default router;
