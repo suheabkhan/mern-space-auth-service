e23a10515655a99924ec00ab9714d4eb47d0aa90
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-02-10 18:00:31 +0530

    setup test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,14 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: true,
+    // do u want the code coverage
+    collectCoverage: true,
+    //node is v8 engine
+    coverageProvider: 'v8',
+    //collect coverage from which files, basically from scr folder and dont include tests and node_modules
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!tests/**/*.spec.ts',
+        '!**/node_modules/**',
+    ],
 };
