6ebf25cafdbdf886892fb050f23a3f5fa216b3ef
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-02-12 16:41:22 +0530

    removed comment

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ import cors from 'cors';
 const app = express();
 app.use(
     cors({
-        //todo : move to .env file
         origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
         credentials: true,
         methods: 'GET,POST,PUT,PATCH,DELETE',
